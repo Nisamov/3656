cb88af3ebf7a4b7d1e1e0a68262943d9c84b88fe
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-04-15 19:46:39 +0200

    .

--- programs/3656.sh
+++ programs/3656.sh
@@ -3,7 +3,7 @@
 # Route config file
 CONFIG_ROUTE="/usr/local/sbin/3656/configured/config.conf"
 
-# If Config file does not exists
+# Check for file to exist
 if [ ! -f "$CONFIG_ROUTE" ]; then
     echo "[#] Config file does not exist"
     # Call program to repair file
@@ -13,14 +13,14 @@ fi
 # Leer el archivo línea por línea
 while IFS= read -r linea; do
     # Verificar si la línea contiene "input=yes"
-    if [[ $linea == *"input=yes"* ]]; then
-        # Acción si input=yes
-        echo "La acción para input=yes aquí"
+    if [[ $linea == *"config_enabled=true"* ]]; then
+        # If file config is enabled
+        echo "File is ready to read"
     fi
     
     # Verificar si la línea contiene "input=no"
-    if [[ $linea == *"input=no"* ]]; then
-        # Acción si input=no
-        echo "La acción para input=no aquí"
+    if [[ $linea == *"config_enabled=false"* ]]; then
+        # File is not enabled
+        echo "File is not ready to read"
     fi
 done < "$ruta_del_archivo"
